38c7a1da9126068d8917b2cbb38788f366de9716
KshitijKoli <kshitijparmeshwar73@gmail.com>
2026-01-19 22:58:45 +0530

    Update Day19.py

--- Day19.py
+++ Day19.py
@@ -37,3 +37,57 @@ class School:
     def __init__(self):
         print("My name is Kshitij")
 a=School()
+
+#create student class that takes name and marks of 3 subjects as a arg in constructor. Then create an method to print an avg
+class Student:
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+        print(f"{self.name} has {self.marks} marks")
+    def avg(self):
+        sum=0 
+        for i in self.marks:
+            sum+=i
+        print(f"{self.name} your avg marks are :",sum/3)
+std1=Student("Kshitij",[28,29,40])
+std1.avg()
+
+##create student class that takes name and marks of 3 subjects as a arg in constructor. Then create an method to print an avg
+
+class Std:
+    @staticmethod
+    def clg(name):
+        print(name)
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+    def avg(self):
+        sum=0
+        for i in self.marks:
+            sum+=i
+        print(f"{self.name} your avg is :",sum/len(self.marks))
+s1=Std("KK",[28,29,40])
+s1.avg()
+s1.clg("DYP")
+
+class Account:
+    def __init__(self,balance,acc_no):
+        self.balance=balance
+        self.acc_no=acc_no
+
+    def debit(self,amt):
+        self.balance-=amt
+        print("Amount debited:",amt)
+        print("Amount Remaining:",self.balance)
+
+    def credit(self,amt):
+        self.balance+=amt
+        print("amount credited:",amt)
+        print("Total Amount Remaining",self.balance)
+        
+pr1=Account(10000,72671638)
+print("Current balance is:",pr1.balance)
+print("Acc no is:",pr1.acc_no)
+pr1.debit(100)
+pr1.credit(2000)
+
